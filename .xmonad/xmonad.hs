eb887404918431cf2d8e971436f699e1e3354700
Kostis Karantias <kkarantias@gmail.com>
2018-11-06 10:20:59 +0200

    basic xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Config.Desktop (desktopConfig)
+import XMonad.Util.EZConfig (additionalKeysP)
+
+audioKeys = [
+    ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+    ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+    ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle") ]
+
+extraKeys = audioKeys
+
+main = xmonad $ desktopConfig
+    { terminal    = "alacritty"
+    , modMask     = mod4Mask
+    , borderWidth = 3
+    } `additionalKeysP` extraKeys
