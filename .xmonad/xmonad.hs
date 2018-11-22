9a0eae8582ee7c10883326d40b2515282de25d23
Kostis Karantias <kkarantias@gmail.com>
2018-11-22 23:55:46 +0200

    xmonad now able to control brightness

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,12 +5,13 @@ import XMonad.Util.EZConfig (additionalKeysP)
 audioKeys = [
     ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%"),
     ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%"),
-    ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle") ]
+    ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
+    ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10"),
+    ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10") ]
 
 extraKeys = audioKeys
 
 main = xmonad $ desktopConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
-    , borderWidth = 3
     } `additionalKeysP` extraKeys
